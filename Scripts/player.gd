681d2078654b309ec980ba04877cf1b2983118c1
Eduarda Olímpio <93235055+MEduardaOl@users.noreply.github.com>
2023-11-23 00:25:06 -0300

    Adding player movement

---
+++ Scripts/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody3D
+
+@export_category("Settings Player")
+@export var speed = 5.0
+@export var jump_force = 4.5
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_force
+
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
